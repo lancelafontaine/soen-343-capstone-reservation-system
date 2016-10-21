0556c59ebac2e771fb2bb817ef7aa534a5b89875
Simz <bitugos@gmail.com>
2016-10-21 18:34:40 -0400

    Transform register into class-based view

--- src/mysite/reservator/views.py
+++ src/mysite/reservator/views.py
@@ -1,29 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
 
-def index(request):
-    return render(request, 'reservator/login.html')
+class Registry(View):
+    # ReservationManager(rm) attribute
 
-def log_in(request):
-    user = authenticate(username=request.POST['username'], password=request.POST['password'])
-    if user is not None:
-        login(request, user)
-        # Redirect to a success
-    else:
-        render(request, 'reservation/index.html')
-        # Return 'invalid' login
+    def index(request):
+        return render(request, 'reservator/login.html')
 
-def log_out(request):
-    logout(request)
+    def log_in(request):
+        # Check if username and password match 
+        # Save current Users in a Session Table
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            login(request, user)
+            # Redirect to a success
+        else:
+            render(request, 'reservation/index.html')
+            # Return 'invalid' login
 
-    # Redirect to a success page
+    def log_out(request):
+        logout(request)
+        # Redirect to a success page
 
-def register(request):
-    print "HELLO"
+    def modifyReservation(request):
+        # rm.modfiyReservation()
+        print "modify"
+
+    def cancelReservation(request):
+        # rm.cancelReservation()
+        print "cancel"
+
+    def makeReservation(request):
+        # rm.makeReservation()
+        print "make"
+    
+    def viewReservations(request):
+        # rm.getReservations(roomNumber);
+        # Display
 
 
-def modify(request):
-    print "HELLO"
