ad79a252d1dc1d8b6d387ad872dba761d775a059
Simz <bitugos@gmail.com>
2016-10-22 23:02:22 -0400

    Remove class-based view

--- src/mysite/reservator/views.py
+++ src/mysite/reservator/views.py
@@ -1,42 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views import View
 
 
-class Registry(View):
-    # ReservationManager(rm) attribute
+# Reservation manager attribute
 
-    def index(request):
-        return render(request, 'reservator/login.html')
+def index(request):
+    return render(request, 'reservator/login.html')
 
-    def log_in(request):
-        # Check if username and password match 
-        user = authenticate(username=request.POST['username'], password=request.POST['password'])
-        if user is not None:
-            login(request, user)
-            # Redirect to a success
-        else:
-            render(request, 'reservation/index.html')
-            # Return 'invalid' login
+def log_in(request):
+    # Check if username and password match 
+    user = authenticate(username=request.POST['username'], password=request.POST['password'])
+    if user is not None:
+        login(request, user)
+        # Redirect to a success
+    else:
+        render(request, 'reservation/index.html')
+        # Return 'invalid' login
 
-    def log_out(request):
-        logout(request)
-        # Redirect to a success page
+def log_out(request):
+    logout(request)
+    # Redirect to a success page
 
-    def modifyReservation(self, request):
-        # rm.modfiyReservation()
-
-
-    def cancelReservation(self, request):
-        # rm.cancelReservation()
+def modifyReservation(self, request):
+    # rm.modfiyReservation()
 
 
-    def makeReservation(self, request):
-        # rm.makeReservation()
+def cancelReservation(self, request):
+    # rm.cancelReservation()
 
-    
-    def viewReservations(self, request):
-        # rm.getReservations(roomNumber);
-        # Display
+
+def makeReservation(self, request):
+    # rm.makeReservation()
+
+
+def viewReservations(self, request):
+    # rm.getReservations(roomNumber);
+    # Display
 
 
