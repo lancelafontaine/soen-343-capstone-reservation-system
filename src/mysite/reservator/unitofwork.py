9dbdc2d315ace475af9c9d477a4365e417817c8a
Simz <bitugos@gmail.com>
2016-10-24 17:14:30 -0400

    Partial UoW implementation

--- src/mysite/reservator/unitofwork.py
+++ src/mysite/reservator/unitofwork.py
@@ -1,10 +1,28 @@
 class UnitOfWork:
+    def __init__(self, mapper):
+        self.mapper = mapper
+        self.newObjects = []
+        self.dirtyObjects = []
+        self.removedObjects = []
 
-    def registerDirty():
-        pass
+    def registerNew(obj):
+        newObjects.append(obj)
 
-    def commit():
-        pass
+    def registerDirty(obj):
+        dirtyObjects.append(obj)
+    
+    def registerRemoved(obj):
+        removedObjects.append(obj)
+
+    def commit(self):
+        for obj in self.newObjects:
+            self.mapper.insert(obj)
+
+        for obj in self.dirtyObjects:
+            self.mapper.update(obj)
+
+        for objc in self.removedObjects:
+            self.mapper.delete(obj)
 
     def rollback():
         pass
