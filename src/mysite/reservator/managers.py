22814211e4090306e32d87c8371da265ec71ffbb
Simz <bitugos@gmail.com>
2016-10-28 19:50:53 -0400

    Create sample database link

--- src/mysite/reservator/managers.py
+++ src/mysite/reservator/managers.py
@@ -1,15 +1,33 @@
 # Contains all 'Managers' for User, Room and Reservation
 
-class ReservationManager:
-	
+class ReservationsManager:
+    maxReservationsPerWeekPerUser = 3
 
-	def makeReservation():
-		pass
+    def makeReservation(username, roomNumber, timeslot):
+        # Cannot make a reservation for the same timeslot in different rooms
+        # If the chosen timeslot is free:
+        #   1. Check maxReservations limit for that week, for that username is less than some number X.
+        #       If maxReservation is less than limit X:
+        #           Instantiate UnitOfWork
+        #           Create a new Reservation instance:
+        #               fields: username, roomNumber, startWeek, timeslot, status=filled, timestamp=now
+        #           Add the new Reservation to ReservationIdentityMap
+        #           Register the new Reservation with UnitOfWork
+        #            
+        #
+        #
+        #
+        #
+        pass
 
-	def modfiyReservation():
-		pass
 
-	def cancelReservation():
+    def modfiyReservation(username, roomNumber, timeslot):
+        # Can change the room of the reservation and the timeslot
+        # But will be put at the back of the waiting list (due to newer timestamp)
+        pass
 
+    def cancelReservation(username, roomNumber, timeslot):
+        pass
 
-	def viewReservations():
+    def getReservations(roomNumber, startWeek):
+        pass
