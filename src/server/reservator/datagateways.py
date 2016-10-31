fa9bb953f692becdd6ad30f1513e5e61e98f2c5c
Simz <bitugos@gmail.com>
2016-10-30 21:26:45 -0400

    Create TDG functions

--- src/server/reservator/datagateways.py
+++ src/server/reservator/datagateways.py
@@ -1,6 +1,18 @@
+from django.db import connection
 
-
+# Warning: Django is set to AUTOCOMMIT mode unless otherwise specified.
 class ReservationTDG:
-	pass
+    
+    def insert(username, roomNumber, timeslot, timestamp):
+        pass
+
+    def delete(username, roomNumber, timeslot, timestamp):
+        pass
+
+    def update(username, roomNumber, timeslot, timestamp):
+        pass
+
+    def find(username, roomNumber, timeslot, timestamp):
+        pass
 
 
