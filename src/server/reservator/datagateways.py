2de0ca9b3715f9cab356efb4768ef2f45647acb5
Simz <bitugos@gmail.com>
2016-10-30 22:49:15 -0400

    Implement insert and delete queries

--- src/server/reservator/datagateways.py
+++ src/server/reservator/datagateways.py
@@ -3,11 +3,17 @@ from django.db import connection
 # Warning: Django is set to AUTOCOMMIT mode unless otherwise specified.
 class ReservationTDG:
     
-    def insert(username, roomNumber, timeslot, timestamp):
-        pass
+    def insert(username, roomNumber, status, timeslot, timestamp):
+        with connection.cursor() as cursor:
+            cursor.execute("INSERT INTO reservations (USER_ID,ROOM_ID,\
+                    STATUS,TIMESLOT,TIMESTAMP) VALUES (SELECT ID from users WHERE USERNAME=%s,\
+                    SELECT ID from rooms WHERE ROOMNUMBER=%s,%s,%s,%s)", [username,roomNumber,status,timeslot,timestamp])
 
-    def delete(username, roomNumber, timeslot, timestamp):
-        pass
+    def delete(username, roomNumber, timeslot):
+        with connection.cursor() as cursor:
+            cursor.execute("DELETE FROM reservations (USER_ID,ROOM_ID,\
+                    STATUS,TIMESLOT,TIMESTAMP) VALUES (SELECT ID from users WHERE USERNAME=%s,\
+                    SELECT ID from rooms WHERE ROOMNUMBER=%s,%s)", [username,roomNumber,timeslot])
 
     def update(username, roomNumber, timeslot, timestamp):
         pass
