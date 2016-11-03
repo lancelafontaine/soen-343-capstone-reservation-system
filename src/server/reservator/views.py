752836dd2d9b81929f3a696ebede0dd888fb531b
Simz <bitugos@gmail.com>
2016-11-02 22:45:01 -0400

    Update views

--- src/server/reservator/views.py
+++ src/server/reservator/views.py
@@ -1,39 +1,36 @@
-#from django.shortcuts import render
-#from django.http import HttpResponse
 from .managers import ReservationsManager
-from django.db import connection
 from django.http import JsonResponse
+
+
 rm = ReservationsManager()
 
+
 def home(request):
-    listy = []
-    with connection.cursor() as cursor:
-        cursor.execute('SELECT symbol from stocks LIMIT 1')
-        for row in cursor:
-            listy.append(row)
     return JsonResponse({'list':'bar'})
+
 
 def log_in(request):
     # request.session['username'] = 'Jack'
     return JsonResponse({'foo':'bar'})
 
+
 def log_out(request):
-    print "log_out"
+    pass
 
 
 def modifyReservation(request):
-    print "modify"
+    pass
 
 
 def cancelReservation(request):
-    print "cancel"
+    pass
 
 
 def makeReservation(request):
-    print "make"
+    pass
 
 
 def viewReservations(request):
-    print "view"
+    pass
 
 
