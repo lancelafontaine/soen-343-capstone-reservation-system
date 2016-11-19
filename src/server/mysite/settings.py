c7fb6835be2c0a3c749ad797939fafcabeb9a773
Lance Lafontaine <lance.lafontaine92@gmail.com>
2016-11-19 17:20:54 -0500

    adds server-side configuration in order to all session cookie storage with cross origin resource sharing

--- src/server/mysite/settings.py
+++ src/server/mysite/settings.py
@@ -50,7 +50,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8080',
+    '127.0.0.1:8080'
+)
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'mysite.urls'
 
