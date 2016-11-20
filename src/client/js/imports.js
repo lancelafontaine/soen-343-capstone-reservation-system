4066e28a93ed2292ba5ae75dca4680f6187dcbda
unknown <bennyzhao@live.ca>
2016-11-19 20:53:54 -0500

    Refactored import.js

--- src/client/js/imports.js
+++ src/client/js/imports.js
@@ -9,28 +9,8 @@ $(document).ready(function(){
     //set sidebar backgoround
     setSideBarConcordia();
 
-    //these codes need to be organized
-    $('input').blur(function() {
-    	var $this = $(this);
-    	if ($this.val()) $this.addClass('used');
-    	else $this.removeClass('used');
-  	});
-  	var $ripples = $('.ripples');
-  	$ripples.on('click.Ripples', function(e) {
-    	var $this = $(this);
-    	var $offset = $this.parent().offset();
-    	var $circle = $this.find('.ripplesCircle');
-    	var x = e.pageX - $offset.left;
-    	var y = e.pageY - $offset.top;
-    	$circle.css({
-      		top: y + 'px',
-      		left: x + 'px'
-    	});
-    	$this.addClass('is-active');
-  	});
-  	$ripples.on('animationend webkitAnimationEnd mozAnimationEnd oanimationend MSAnimationEnd', function(e) {
-  		$(this).removeClass('is-active');
-  	});
+    //set login page animation
+    loginPageAnimation();
 
   	//calendar code
   	var date = new Date();
@@ -77,6 +57,30 @@ function setSideBarConcordia(){
         sidebar_container = '<div class="sidebar-background" style="background-image: url(' + image_src + ') "/>'
         $sidebar.append(sidebar_container);
     }
+}
+
+function loginPageAnimation(){
+    $('input').blur(function() {
+      var $this = $(this);
+      if ($this.val()) $this.addClass('used');
+      else $this.removeClass('used');
+    });
+    var $ripples = $('.ripples');
+    $ripples.on('click.Ripples', function(e) {
+      var $this = $(this);
+      var $offset = $this.parent().offset();
+      var $circle = $this.find('.ripplesCircle');
+      var x = e.pageX - $offset.left;
+      var y = e.pageY - $offset.top;
+      $circle.css({
+          top: y + 'px',
+          left: x + 'px'
+      });
+      $this.addClass('is-active');
+    });
+    $ripples.on('animationend webkitAnimationEnd mozAnimationEnd oanimationend MSAnimationEnd', function(e) {
+      $(this).removeClass('is-active');
+    });
 }
 
 /*
